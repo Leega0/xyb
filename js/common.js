d68722778b3cf61a75e77840a2822114c4e88cd0
Leega0 <igeekes@gmail.com>
2015-07-09 11:48:48 +0800

    借款页对齐修复
    我要借款页面,且所有输入框默认值修改
    onfocus="if(this.value==this.defaultValue)this.value=''"
    onblur="if(this.value.replace(/
    /ig,'')=='')this.value=this.defaultValue"

--- js/common.js
+++ js/common.js
@@ -18,12 +18,6 @@ $(function(){
 		$(".menber-list").stop().slideUp();
 	});
     // 返回顶部
-    $("body").append('<ul class="ui-popbar">'+
-	'<li><a href="" class="ui-kefu icon-popbar"></a></li>'+
-	'<li><a href="" class="ui-code icon-popbar"></a><img src="images/wcode.png" class="ui-qrcode"></li>'+
-	'<li><a href="" class="ui-cal icon-popbar"></a></li>'+
-	'<li><a href="#" class="ui-gotop icon-popbar"></a></li>'
-+'</ul>');
     var $popbar = $('.ui-popbar'),
         $pobarWidth = $popbar.width(),
         barEdge = ($(document).width()-1200) / 2 - $pobarWidth - 15;
@@ -38,55 +32,55 @@ $(function(){
         $wechatImg.css('display', 'none');
     });
 //判断浏览器是否支持placeholder属性
-  supportPlaceholder='placeholder'in document.createElement('input'),
- 
-  placeholder=function(input){
- 
-    var text = input.attr('placeholder'),
-    defaultValue = input.defaultValue;
- 
-    if(!defaultValue){
- 
-      input.val(text).addClass("phcolor");
-    }
- 
-    input.focus(function(){
- 
-      if(input.val() == text){
-   
-        $(this).val("");
-      }
-    });
- 
-  
-    input.blur(function(){
- 
-      if(input.val() == ""){
-       
-        $(this).val(text).addClass("phcolor");
-      }
-    });
- 
+//   supportPlaceholder='placeholder'in document.createElement('input'),
+//  
+//   placeholder=function(input){
+//  
+//     var text = input.attr('placeholder'),
+//     defaultValue = input.defaultValue;
+//  
+//     if(!defaultValue){
+//  
+//       input.val(text).addClass("phcolor");
+//     }
+//  
+//     input.focus(function(){
+//  
+//       if(input.val() == text){
+//    
+//         $(this).val("");
+//       }
+//     });
+//  
+//   
+//     input.blur(function(){
+//  
+//       if(input.val() == ""){
+//        
+//         $(this).val(text).addClass("phcolor");
+//       }
+//     });
+//  
     //输入的字符不为灰色
-    input.keydown(function(){
-  
-      $(this).removeClass("phcolor");
-    });
-  };
+//     input.keydown(function(){
+//   
+//       $(this).removeClass("phcolor");
+//     });
+//   };
  
   //当浏览器不支持placeholder属性时，调用placeholder函数
-  if(!supportPlaceholder){
- 
-    $('input').each(function(){
- 
-      text = $(this).attr("placeholder");
- 
-      if($(this).attr("type") == "text"){
- 
-        placeholder($(this));
-      }
-    });
-  }
+//   if(!supportPlaceholder){
+//  
+//     $('input').each(function(){
+//  
+//       text = $(this).attr("placeholder");
+//  
+//       if($(this).attr("type") == "text"){
+//  
+//         placeholder($(this));
+//       }
+//     });
+//   }
 // 详情页tab切换
 $page = $(document).find('.content').find('div[data-page]');
 $page_radar = $(document).find('.contentb').find('div[data-page]');
