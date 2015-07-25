3c207154136e63f7d1e23e7c791c466ceb3f6824
Leega0 <igeekes@gmail.com>
2015-07-25 18:40:36 +0800

    首页修改版

--- js/index.js
+++ js/index.js
@@ -45,17 +45,35 @@ var scrtime;
          clearInterval(scrtime);
     },function(){
         scrtime = setInterval(function(){
-            	var ul = $("#reviews ul");
-                // var liHeight = ul.find("li:last").height();
-                // ul.animate({marginTop : -(liHeight+70) +"px"},1000,function(){
-                // 	ul.find("li:last").prependTo(ul)
-                // 	ul.find("li:first").hide();              	 
-                // 	ul.css({marginTop:0});
-                // 	ul.find("li:first").show();
-                // });
-                ul.find("li:last").prependTo(ul) ;
-                ul.find("li:first").animate({opacity:1}, 500);
-                ul.find("li:last").animate({opacity:0}, 500);       
+              var ul = $("#reviews ul");
+                var liHeight = ul.find("li:last").height();
+                ul.animate({marginTop : liHeight+40 +"px"},1000,function(){
+                  ul.find("li:last").prependTo(ul)
+                  ul.find("li:first").hide();
+                  ul.css({marginTop:0});
+                  ul.find("li:first").fadeIn(1000);
+                });        
         },3000);
      }).trigger("mouseleave");
+  });
+// 用户评论
+$("#write_review").focus(function(){
+   $(".text-num-last").show(300);
+   //获取输入字数
+   $(this).keyup(function() {
+      //获取输入框内的文字长度
+      var numL = $(this).val().length;
+      _val=$(this).val(); 
+      if (numL <= 120) {
+        numL = 120 - numL;
+        $("#text_num").html(numL)
+      }
+      else if(numL>121){
+        $(this).val(_val.substring(0,120)); 
+         //$(".text-num-last").html('不能再写多啦~'+'<i class="ui-icon-triangle-red-bottom"></i>')
+      }
+   });
+});
+$("#write_review").blur(function(){
+  $(".text-num-last").hide(300);
 });
