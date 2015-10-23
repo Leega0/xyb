42ac0e0a648de0f91631fbfb63ed41e1b5de019d
Leega0 <igeekes@gmail.com>
2015-10-23 15:50:43 +0800

    old
    新版待上

--- js/index.js
+++ js/index.js
@@ -19,40 +19,7 @@ $(function(){
        return false;
   }); 
    }
-   onSliderLoad();
-  	// countdown
-
-  	var ts1 = '2015-07-01 17:40:02';
-  	var ts2 = '2015-08-10 11:00:00.0';
-  	var da =ts1.replace("年","-").replace("月","-").replace("日","").replace(/-/g,"/").split(/\/|\:|\ /);
-  	systemTime = new Date(da[0], da[1] - 1, da[2], da[3], da[4], da[5]);
-  	var da2 =ts2.replace("年","-").replace("月","-").replace("日","").replace(/-/g,"/").split(/\/|\:|\ /);
-  	activitiDate = new Date(da2[0], da2[1] - 1, da2[2], da2[3], da2[4], da2[5]);
-  	if(systemTime < activitiDate){ 
-  		$('#timedown').countdown({
-  			timestamp: activitiDate,
-  			timeNow: systemTime,
-  			callback: function(days, hours, minutes, seconds) {
-  				if(days<=0&&hours <= 0 && minutes <= 0 && seconds <= 0) {
-  					$('.a_now').show();
-  					$('.a_before').hide();
-  					$('.a_after').hide();
-  				}	
-  			}
-  		});
-  		$('.a_now').hide();
-  		$('.a_before').show();
-  		$('.a_after').hide();
-  	} else if (systemTime > activitiDate) {
-  		$('.a_now').show();
-  		$('.a_before').hide();
-  		$('.a_after').hide();
-  	};
-  	if (false) {
-  		$('.a_now').hide();
-  		$('.a_before').hide();
-  		$('.a_after').show();
-  	}
+   //onSliderLoad();
 // 用户评论滚动效果
 var scrtime;
     $("#reviews").hover(function(){
